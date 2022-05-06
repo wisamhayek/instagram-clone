31a09abb8fd9f90653e36fa721d92dde7b53b8ac
Wisam Hayek <hayekwisam@gmail.com>
2022-05-05 23:33:17 -0700

    Updates

---
+++ static/js/934.4566a9e7.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkinstagram_clone=self.webpackChunkinstagram_clone||[]).push([[934],{2934:function(n,t,e){e.r(t),e.d(t,{default:function(){return a}});var u=e(2791),c=e(184);function a(){return(0,u.useEffect)((function(){document.title="Not found - Instagram"}),[]),(0,c.jsx)("div",{children:"Page not found"})}}}]);
+//# sourceMappingURL=934.4566a9e7.chunk.js.map
